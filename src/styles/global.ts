7f20cf6923fc56cd9844abcb4235d508805cacff
Richard Lemos <eternal.cassola@gmail.com>
2023-06-13 13:58:03 -0300

    feat: initial commit

---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import { globalCss } from "./stitches.config";
+
+export const globalStyles = globalCss({
+    '*': {
+        margin: 0,
+        padding: 0,
+    },
+
+    body: {
+        '-webkit-font-smoothing': 'antialiased',
+        background: "$black",
+        color: '$white'
+    },
+
+    'body, input, texstarea, button': {
+
+    }
+})
