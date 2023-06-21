9b8f8e5bfd785ca4d84a05cb3a4aaba1c061b3c5
Richard Lemos <eternal.cassola@gmail.com>
2023-06-21 13:06:50 -0300

    config(pages): resolving routes 404

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
-=======
->>>>>>> 98b04ac97ea8337365f04fa324ef4370da126efb
   reactStrictMode: true,
-  pageExtensions: ['jsx', 'js', 'tsx', 'ts'],
 }
 
 module.exports = nextConfig
