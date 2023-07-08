31a348cf8e6df53c6e6aa989c2578cc3bcaabe27
Richard Lemos <eternal.cassola@gmail.com>
2023-07-08 16:33:53 -0300

    ui/ux (progressbar): setting transition duration equal to GamesIcons

--- src/components/ProgressBar/styles.ts
+++ src/components/ProgressBar/styles.ts
@@ -3,5 +3,5 @@ import { styled } from '@stitches/react'
 export const ProgressBarContainer = styled('div', {
   height: '3px',
   background: '$blue-200',
-  transition: ' 1s all',
+  transition: ' .50s all',
 })
